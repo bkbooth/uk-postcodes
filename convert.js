a469fa7f765c6f90f1263208bbd6279d0cbb107e
Ben Booth <bkbooth@gmail.com>
2014-11-27 21:17:48 +1100

    Add 'precision' option to convert.js

--- convert.js
+++ convert.js
@@ -1,16 +1,25 @@
 var argv = require('minimist')(process.argv.slice(2)),
     fs = require('fs'),
     path = require('path'),
-    ogr2ogr = require('ogr2ogr');
+    ogr2ogr = require('ogr2ogr'),
+    ogr2ogrOptions = [];
 
 if (argv.help || argv.h || !(argv.i && argv.o)) {
-    console.log("Usage: node convert -i {input_file} -o {output_file}");
+    console.log("Usage: node convert -i {input_file} -o {output_file} [-p {precision}]");
     process.exit(1);
 }
+
+// Setup default decimal prevision to 3
+var precision =
+    (argv.p && !isNaN(parseInt(argv.p))) ? parseInt(argv.p) :
+    (argv.precision && !isNaN(parseInt(argv.precision))) ? parseInt(argv.precision) :
+    3;
+ogr2ogrOptions.push('-lco', 'COORDINATE_PRECISION='+precision);
 
 console.log("Reading '"+argv.i+"'");
 
 ogr2ogr(path.join(__dirname, argv.i))
+    .options(ogr2ogrOptions)
     .exec(function(err, data) {
         if (err) {
             console.error("Error reading input file.");
