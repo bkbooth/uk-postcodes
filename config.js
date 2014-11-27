37cd4729a9e31a38691918bf9d3dd7be6b2bec64
Ben Booth <bkbooth@gmail.com>
2014-11-28 10:12:56 +1100

    Add OpenStreetMap as default tiling provider
    - Comment JavaScript

--- config.js
+++ config.js
@@ -1,8 +1,12 @@
 var config = {
-    mapBox: {
+    openStreetMap: {
+        url: 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+        attribution: "&copy; <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> contributors"
+    },
+    mapbox: {
         url: 'http://{s}.tiles.mapbox.com/v3/{id}/{z}/{x}/{y}.png',
         id: 'your.mapbox.id',
-        attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>'
+        attribution: "<a href='https://www.mapbox.com/about/maps/' target='_blank'>&copy; Mapbox &copy; OpenStreetMap</a> <a class='mapbox-improve-map' href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a>"
     },
     fileAreas: 'data/Areas.geojson',
     fileDistricts: 'data/Districts.geojson',
